7ade88adceb29613942ddea4ff690e6953d9a2e3
Xientra <38855570+Xientra@users.noreply.github.com>
2021-11-12 16:45:57 +0100

    Added Everything

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections; // <- IEnumerator is from this
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField]
+	private float speed = 1f;
+
+	public Material defaultMaterial;
+	public Material redMaterial;
+
+	private Rigidbody rb;
+	private MeshRenderer mr;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		mr = GetComponent<MeshRenderer>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.W))
+			rb.AddForce(0, 0, speed);
+		if (Input.GetKey(KeyCode.A))
+			rb.AddForce(-speed, 0, 0);
+		if (Input.GetKey(KeyCode.S))
+			rb.AddForce(0, 0, -speed);
+		if (Input.GetKey(KeyCode.D))
+			rb.AddForce(speed, 0, 0);
+	}
+
+	public void GetHit()
+	{
+		StartCoroutine(BecomeRed(3));
+	}
+
+	public IEnumerator BecomeRed(float duration)
+	{
+		mr.material = redMaterial;
+		yield return new WaitForSeconds(duration);
+		mr.material = defaultMaterial;
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	public float speed = 20;
+
+	private void Update()
+	{
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+			other.GetComponent<PlayerMovement>().GetHit();
+
+		// Destroy only after 3 seconds so the trail can dissapear nicely
+		speed = 0;
+		GetComponent<Collider>().enabled = false;
+		Destroy(gameObject, 3);
+	}
+}
